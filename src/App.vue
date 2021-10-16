211e08adbf8a3c53e290ecade2f25f4fbf71f599
richardingwe <richardingwe5@gmail.com>
2021-10-16 13:20:07 +0100

    made sources a component

--- src/App.vue
+++ src/App.vue
@@ -4,31 +4,21 @@
 		<router-view :key="$route.fullPath" />
 		<!-- Sidebar Section -->
 		<aside class="w-full md:w-1/3 flex flex-col items-center px-3">
-			<div class="w-full bg-white shadow flex flex-col my-4 p-6">
-				<p class="text-xl font-semibold pb-5">News Sources</p>
-				<div class="grid grid-cols-1 gap-3 text-white">
-					<router-link
-						v-for="source of sources"
-						:key="source.id"
-						:to="{ name: 'Source', params: { source: source.id } }"
-						class="hover:bg-blue-700 bg-blue-800 rounded py-2 px-4 mx-2"
-					>
-						{{ source.name }}
-					</router-link>
-				</div>
-			</div>
+			<Sources :sources="sources" />
 		</aside>
 	</div>
 </template>
 
 <script>
 	import Header from '@/components/Header';
+	import Sources from '@/components/Sources';
 	import axios from 'axios';
 
 	export default {
 		name: 'App',
 		components: {
 			Header,
+			Sources,
 		},
 		data() {
 			return {
---
+++ src/components/Sources.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="w-full bg-white shadow flex flex-col my-4 p-6">
+		<p class="text-xl font-semibold pb-5">News Sources</p>
+		<div class="grid grid-cols-1 gap-3 text-white">
+			<router-link
+				v-for="source of sources"
+				:key="source.id"
+				:to="{ name: 'Source', params: { source: source.id } }"
+				class="hover:bg-blue-700 bg-blue-800 rounded py-2 px-4 mx-2"
+			>
+				{{ source.name }}
+			</router-link>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Source',
+		props: ['sources'],
+	};
+</script>
